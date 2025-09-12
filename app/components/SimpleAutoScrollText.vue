8b7cf877b14d78b34d386579270e2f6f8edbe285
samithseu <samithseu2002@gmail.com>
2025-09-12 22:57:55 +0700

    [chore]: pressing `Enter` to copy when focusing on email

--- app/components/SimpleAutoScrollText.vue
+++ app/components/SimpleAutoScrollText.vue
@@ -10,6 +10,7 @@ const props = defineProps<{
     class="overflow-x-auto no-scrollbar h-max cursor-copy py-1.5 px-3 select-all"
     :class="props.classes"
     @click="props.copyCallback && props.copyCallback()"
+    @keydown.enter="props.copyCallback && props.copyCallback()"
   >
     <code class="text-nowrap"><slot /></code>
   </div>
