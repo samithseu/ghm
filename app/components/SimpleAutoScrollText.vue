6e296d318f9b01a9aed9f765e0f54abfe487c4b6
samithseu <samithseu2002@gmail.com>
2025-09-13 15:21:55 +0700

    [ui]: added `Footer`

--- app/components/SimpleAutoScrollText.vue
+++ app/components/SimpleAutoScrollText.vue
@@ -7,7 +7,7 @@ const props = defineProps<{
 
 <template>
   <div
-    class="overflow-x-auto no-scrollbar h-max cursor-copy py-1.5 px-3 select-all"
+    class="w-full overflow-hidden overflow-x-auto no-scrollbar h-max cursor-copy py-1.5 px-3 select-all scroll-smooth"
     :class="props.classes"
     @click="props.copyCallback && props.copyCallback()"
     @keydown.enter="props.copyCallback && props.copyCallback()"
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,25 @@
 <template>
   <div
-    class="bg-white dark:bg-[#0a0a0a] w-full min-h-dvh flex flex-col justify-center items-center px-4 sm:px-0"
+    class="bg-white dark:bg-dark w-full min-h-dvh flex flex-col justify-center items-center px-4 sm:px-0 relative"
   >
     <slot />
+    <footer
+      class="text-gray-500 dark:text-white/40 absolute bottom-0 left-0 py-2 w-full flex justify-center items-center *:hover:text-black *:dark:hover:text-white *:hover:underline *:text-sm"
+    >
+      <NuxtLink title="source code" to="https://github.com/samithseu/ghm/"
+        >source
+      </NuxtLink>
+      &nbsp;&middot;&nbsp;
+      <NuxtLink
+        class="flex items-center gap-1"
+        title="Nuxt.js"
+        to="https://nuxt.com"
+        >made with <Icon size="18" name="lineicons:nuxt" />
+      </NuxtLink>
+      &nbsp;&middot;&nbsp;
+      <NuxtLink title="@samithseu" to="https://github.com/samithseu">
+        by @samithseu
+      </NuxtLink>
+    </footer>
   </div>
 </template>
