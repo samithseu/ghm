92f503df8e74da2bb1036713720069c9f00b797a
samithseu <samithseu2002@gmail.com>
2026-01-31 16:47:52 +0700

    [ui]: removed selection when clicking on email

--- app/components/SimpleAutoScrollText.vue
+++ app/components/SimpleAutoScrollText.vue
@@ -7,7 +7,7 @@ const props = defineProps<{
 
 <template>
   <div
-    class="w-full overflow-hidden overflow-x-auto no-scrollbar h-max cursor-copy py-1.5 px-3 select-all scroll-smooth"
+    class="w-full overflow-hidden overflow-x-auto no-scrollbar h-max cursor-copy py-1.5 px-3 select-none scroll-smooth"
     :class="props.classes"
     @click="props.copyCallback && props.copyCallback()"
     @keydown.enter="props.copyCallback && props.copyCallback()"
